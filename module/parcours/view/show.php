08144d5b03187f069c70eabdea4f3a4b4e064780
LLFT <loic.laffont@gmail.com>
2015-09-22 08:59:26 +0200

    Désactivation du bouton d'ajout signaleur dans la vue des parcours

--- module/parcours/view/show.php
+++ module/parcours/view/show.php
@@ -8,7 +8,7 @@
 
 <div class="form-group">
     <div class="col-sm-offset-2 col-sm-2">
-    <a class="btn btn-success" href="<?php echo $this->getLink('parcours::ajoutSignaleur',array( 'id'=>$this->oParcours->getId(), 'idEvent'=>_root::getParam('idEvent') ) )?>">Ajouter les signaleurs</a>
+    <!--<a class="btn btn-success" href="<?php // echo $this->getLink('parcours::ajoutSignaleur',array( 'id'=>$this->oParcours->getId(), 'idEvent'=>_root::getParam('idEvent') ) )?>">Ajouter les signaleurs</a>-->
     </div>
     <div class="col-sm-offset-2 col-sm-2">
     <a class="btn btn-danger" href="<?php echo $this->getLink('parcours::delete',array( 'id'=>$this->oParcours->getId(), 'idEvent'=>_root::getParam('idEvent') ) )?>">Supprimer</a>
